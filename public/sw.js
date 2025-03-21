0e7b3875dc8cc98d42ad1201f75b607e5fa52fb1
masknetdesign <masknetdesign@gmail.com>
2025-03-21 20:46:25 -0300

    first commit

---
+++ public/sw.js
@@ -0,0 +1,40 @@
+const CACHE_NAME = 'lembrete-v1';
+const urlsToCache = [
+  '/',
+  '/index.html',
+  '/manifest.json',
+  '/src/App.tsx',
+  '/src/main.tsx',
+  '/src/index.css',
+  '/src/App.css'
+];
+
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then((cache) => cache.addAll(urlsToCache))
+  );
+});
+
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request)
+      .then((response) => {
+        if (response) {
+          return response;
+        }
+        return fetch(event.request)
+          .then((response) => {
+            if (!response || response.status !== 200 || response.type !== 'basic') {
+              return response;
+            }
+            const responseToCache = response.clone();
+            caches.open(CACHE_NAME)
+              .then((cache) => {
+                cache.put(event.request, responseToCache);
+              });
+            return response;
+          });
+      })
+  );
+}); 
